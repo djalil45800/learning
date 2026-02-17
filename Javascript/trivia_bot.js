1fea82cd228e34fb7dd1ead12cfe7b83dd826853
oxydit <sahraoui.abdeldjalil@outlook.com>
2026-02-17 08:06:30 +0100

    feat: build a trivia bot

---
+++ Javascript/trivia_bot.js
@@ -0,0 +1,25 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "Trivia";
+let botLocation = "France";
+let favoritLanguage = "Rust";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorit programming language is " + favoritLanguage + ".");
+
+let codingFact = favoritLanguage + " is named after a Fungus (not Metal).";
+console.log(codingFact);
+
+codingFact = favoritLanguage + "'s most hated enemy is its compler.";
+console.log(codingFact);
+
+codingFact = favoritLanguage + " was created wearing pyjamas!";
+console.log(codingFact);
+
+console.log(
+	"It was fun sharing these facts with you. Goodbye! - " +
+		botName +
+		" from " +
+		botLocation +
+		".",
+);
